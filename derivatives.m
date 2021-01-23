7f4c340cbed9f1262cbb086601d58ad830227bf3
santoshrajkumar <rajkumar.nits@gmail.com>
2021-01-23 18:46:43 -0500

    1

---
+++ derivatives.m
@@ -0,0 +1,28 @@
+%% Derivatives
+%---------------S.M. Rajkumar----------------
+
+%% compute derivative of symbolic function
+syms x
+f = sin(x);
+df = diff(f);
+
+subplot(211)
+fplot(f, 'linewidth',2.5), hold on
+fplot(df, 'linewidth',2.5)
+legend(['f(x) =' char(f)], ['df= ' char(df)])
+
+%find value of the function at a specified point
+x1 = pi/3;
+subs(f,x1)
+subs(df,x1)
+
+%% Compute derivative with numeric variable
+Ts = 0.001 %sample time
+t = -6:Ts:6;
+q = sin(t);
+dq = diff(q)*(1/Ts); % need to multiply 1/Ts for scalling purpose
+
+subplot(212)
+plot(t,q,'linewidth',2.5), hold on
+plot(t(1:end-1),dq,'linewidth',2.5)
+legend('q(t)', 'dq')
