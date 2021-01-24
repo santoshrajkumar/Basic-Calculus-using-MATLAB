c98c22c6d528973e09367a39cef1163fcdbf2d8f
santoshrajkumar <rajkumar.nits@gmail.com>
2021-01-23 23:15:00 -0500

    1

--- ode45/massInOil.m
+++ ode45/massInOil.m
@@ -1,9 +1,4 @@
-function yPrime = massInOil(t,y)
-
-c = 4;
-k=4;
-m=1;
-
+function yPrime = massInOil(t,y,c,k,m)
 yPrime = zeros(2,1);
 yPrime(1)= y(2);
 yPrime(2) = -k*y(1)./m -c*y(2)./m ;
--- ode45/massInOil_script.m
+++ ode45/massInOil_script.m
@@ -1,7 +1,13 @@
 % Solve the equation from massInOil.m file
+
+c = input('Enter value of c :  ');
+k=input('Enter value of c :  ');
+m=input('Enter value of c :  ');
+
 tspan = [0 10];
 y0 = [1 0];
 
-[t y] = ode45(@massInOil, tspan, y0);
+% extra parameters c,k,m are passed into the ode function
+[t,y] = ode45(@(t,y) massInOil(t,y,c,k,m), tspan, y0);
 
 plot(t,y(:,1),'linewidth', 2.5)
